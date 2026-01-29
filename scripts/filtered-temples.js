95988873cb34493222a0eb0e447b8d501d6f698b
lizzezzie <lizezzie@gmail.com>
2026-01-29 13:16:44 +0300

    css fix

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -21,7 +21,7 @@ const temples = [
   {
     templeName: "Aba Nigeria",
     location: "Aba, Nigeria",
-    dedicated: "2005, August, 7",
+    dedicated: "2005-08-07",
     area: 11500,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
@@ -29,7 +29,7 @@ const temples = [
   {
     templeName: "Manti Utah",
     location: "Manti, Utah, United States",
-    dedicated: "1888, May, 21",
+    dedicated: "1888-05-21",
     area: 74792,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
@@ -37,7 +37,7 @@ const temples = [
   {
     templeName: "Payson Utah",
     location: "Payson, Utah, United States",
-    dedicated: "2015, June, 7",
+    dedicated: "2015-06-07",
     area: 96630,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
@@ -45,7 +45,7 @@ const temples = [
   {
     templeName: "Yigo Guam",
     location: "Yigo, Guam",
-    dedicated: "2020, May, 2",
+    dedicated: "2020-05-02",
     area: 6861,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
@@ -53,7 +53,7 @@ const temples = [
   {
     templeName: "Washington D.C.",
     location: "Kensington, Maryland, United States",
-    dedicated: "1974, November, 19",
+    dedicated: "1974-11-19",
     area: 156558,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
@@ -61,7 +61,7 @@ const temples = [
   {
     templeName: "Lima Perú",
     location: "Lima, Perú",
-    dedicated: "1986, January, 10",
+    dedicated: "1986-01-10",
     area: 9600,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
@@ -69,7 +69,7 @@ const temples = [
   {
     templeName: "Mexico City Mexico",
     location: "Mexico City, Mexico",
-    dedicated: "1983, December, 2",
+    dedicated: "1983-12-02",
     area: 116642,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
@@ -77,7 +77,7 @@ const temples = [
   {
     templeName: "Tokyo Japan",
     location: "Tokyo, Japan",
-    dedicated: "2017, April, 14",
+    dedicated: "2017-04-14",
     area: 5000,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/tokyo-japan/400x250/tokyo-japan-temple-lds-273999-wallpaper.jpg"
@@ -85,7 +85,7 @@ const temples = [
   {
     templeName: "Bern Switzerland",
     location: "Bern, Switzerland",
-    dedicated: "2017, April, 14",
+    dedicated: "2017-04-14",
     area: 5000,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/bern-switzerland/400x250/bern-switzerland-temple-lds-273999-wallpaper.jpg"
@@ -93,13 +93,15 @@ const temples = [
   {
     templeName: "Kyiv Ukraine",
     location: "Kyiv, Ukraine",
-    dedicated: "2010, August, 29",
+    dedicated: "2010-08-29",
     area: 5000,
     imageUrl:
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/kyiv-ukraine/400x250/kyiv-ukraine-temple-lds-273999-wallpaper.jpg"
   }
 ];
+
 // Function to display temples
+
 const templeContainer = document.getElementById("temples");
 
 function displayTemples(templeArray) {
