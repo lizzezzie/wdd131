e4355c5dda070233badb9db42e07d5e844dc9d9b
lizzezzie <lizezzie@gmail.com>
2026-02-06 23:18:56 +0300

    removed products from form js

--- scripts/form.js
+++ scripts/form.js
@@ -1,37 +1,16 @@
 
-const products = [
-  { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
-  { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-  { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
-  { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-  { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
-];
-
-document.addEventListener("DOMContentLoaded", () => {
-
-  // Populate product select
-  const productSelect = document.getElementById("product");
-  if (productSelect) {
-    products.forEach(product => {
-      const option = document.createElement("option");
-      option.value = product.id;
-      option.textContent = product.name;
-      productSelect.appendChild(option);
-    });
-  }
+  document.addEventListener("DOMContentLoaded", () => {
 
   // Footer year
-  const yearSpan = document.getElementById("currentyear");
-  if (yearSpan) {
-    yearSpan.textContent = new Date().getFullYear();
+  const year = document.getElementById("currentyear");
+  if (year) {
+    year.textContent = new Date().getFullYear();
   }
 
   // Footer last modified
-  const lastModifiedP = document.getElementById("lastModified");
-  if (lastModifiedP) {
-    lastModifiedP.textContent =
-      "Last modified: " + document.lastModified;
+  const modified = document.getElementById("lastModified");
+  if (modified) {
+    modified.textContent = "Last modified: " + document.lastModified;
   }
 
 });
-
